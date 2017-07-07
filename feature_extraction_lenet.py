d7200b7744f7be979875357c8c128bd1d9dd0f9d
wangy12 <wyh804@gmail.com>
2017-07-07 13:01:44 -0400

    first commit

---
+++ feature_extraction_lenet.py
@@ -0,0 +1,190 @@
+import pickle
+import time
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import numpy as np
+from scipy.misc import imread
+
+from keras.datasets import cifar10
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# y_train.shape is 2d, (50000, 1). While Keras is smart enough to handle this
+# it's a good idea to flatten the array.
+y_train = y_train.reshape(-1)
+y_test = y_test.reshape(-1)
+
+n_classes = 10
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.3, random_state = 42, stratify = y_train)
+
+
+# Normalizes the data
+def normalize(img):
+    return (img - 128) / 128
+    
+
+# Normalize
+X_train = normalize(X_train)
+
+#print(X_train.shape[1:4])
+
+# Normalize
+X_valid = normalize(X_valid)
+
+# Normalize
+X_test = normalize(X_test)
+
+
+epochs = 50
+batch_size = 256
+
+from tensorflow.contrib.layers import flatten
+
+def LeNet(x):    
+    
+    mu = 0
+    sigma = 0.1
+    
+    # SOLUTION: Layer 1: Convolutional. 
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # SOLUTION: Activation.
+    conv1 = tf.nn.relu(conv1)
+    #conv1 = tf.nn.softmax(conv1)
+    #conv1 = tf.sigmoid(conv1)
+    #conv1 = tf.tanh(conv1)
+
+	
+    
+    # SOLUTION: Pooling. 
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    #conv1 = tf.nn.avg_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    
+    
+    # SOLUTION: Layer 2: Convolutional. 
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 20), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(20))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # SOLUTION: Activation.
+    conv2 = tf.nn.relu(conv2)
+    #conv2 = tf.nn.softmax(conv2)
+    #conv2 = tf.sigmoid(conv2)
+    #conv2 = tf.tanh(conv2)
+    
+    
+    
+    # SOLUTION: Pooling. 
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    #conv2 = tf.nn.avg_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # SOLUTION: Flatten. 
+    fc0   = flatten(conv2)
+    
+    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(500, 200), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(200))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    # SOLUTION: Activation.
+    fc1    = tf.nn.relu(fc1)
+    #fc1 = tf.nn.softmax(fc1)
+    #fc1 = tf.sigmoid(fc1)
+    #fc1 = tf.tanh(fc1)
+
+    # Dropout: prevent overfitting
+    fc1 = tf.nn.dropout(fc1, keep_prob)
+    
+    # SOLUTION: Layer 4: Fully Connected. Input = 200. Output = 120.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(200, 100), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(100))
+    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    
+    # SOLUTION: Activation.
+    fc2    = tf.nn.relu(fc2)
+    #fc2 = tf.nn.softmax(fc2)
+    #fc2 = tf.sigmoid(fc2)
+    #fc2 = tf.tanh(fc2)
+    
+    # Dropout: prevent overfitting
+    fc2 = tf.nn.dropout(fc2, keep_prob)
+    
+    # SOLUTION: Layer 5: Fully Connected. Input = 120. Output = n_classes or 43.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(100, n_classes), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(n_classes))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+    
+    return logits
+
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, n_classes)
+keep_prob = tf.placeholder(tf.float32)
+
+rate = 0.004
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, batch_size):
+        batch_x, batch_y = X_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+    
+t_start = time.time()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    print("Training...")
+    print()
+    for i in range(epochs):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, batch_size):
+            end = offset + batch_size
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.7})
+            
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './lenet')
+    print("Model saved")
+    
+    
+duration = time.time() - t_start
+
+print(duration)
+
+
+'''
+# test
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = sess.run(accuracy_operation, feed_dict={x: X_test, y: y_test, keep_prob : 1.0})
+
+print('Test Accuracy: {}'.format(test_accuracy))
+'''
